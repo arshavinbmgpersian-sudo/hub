229149d1a0e07ec36730e3ea26630350617d80da
arshavinbmgpersian-sudo <arshavinbmgpersian@gmail.com>
2026-06-09 03:42:38 +0330

    Update check.js

--- functions/check.js
+++ functions/check.js
@@ -1,1 +1,34 @@
+export async function onRequestPost(context) {
 
+  const { code } = await context.request.json();
+
+  const user = await context.env.DB
+    .prepare(
+      "SELECT * FROM users WHERE code = ?"
+    )
+    .bind(code)
+    .first();
+
+  if (!user) {
+    return Response.json({
+      valid: false,
+      message: "کد اشتراک نامعتبر است"
+    });
+  }
+
+  const now = new Date();
+  const expire = new Date(user.expire_date);
+
+  if (now > expire) {
+    return Response.json({
+      valid: false,
+      message: "اشتراک شما منقضی شده است"
+    });
+  }
+
+  return Response.json({
+    valid: true,
+    message: "اشتراک فعال است"
+  });
+
+}
